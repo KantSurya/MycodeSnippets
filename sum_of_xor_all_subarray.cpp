631f273b89b4c12ef74f5f8c4ebf87f06fdbe3d9
SuryaKant <55610604+suryaskant@users.noreply.github.com>
2020-05-24 20:07:42 +0530

    Sum of xor values of all subarray of given array
    Given an array containing N positive integers, the task is to find the sum of XOR of all sub-arrays of the array.

---
+++ sum_of_xor_all_subarray.cpp
@@ -0,0 +1,52 @@
+/*
+    Problem Statement : https://www.codechef.com/COOK39/problems/PPXOR
+    Tutorial : https://www.geeksforgeeks.org/sum-of-xor-of-all-subarrays/
+    sum of xor of all subarrays
+    Xor, Subarray, Bit manipulation,    
+*/
+
+#include<iostream>
+using namespace std;
+void XorSumSubarrays(int arr[],int n)
+{
+    long long sum=0;
+    for(int i=0;i<32;++i)
+    {
+        int x=(1<<i);
+        
+        int c_odd=0;
+        bool odd=false;
+        
+        for(int j=0;j<n;++j)
+        {
+            if(arr[j]&x)
+            odd=(!odd);
+            if(odd)
+            c_odd++;
+        }
+        
+        for(int j=0;j<n;++j)
+        {
+            sum+=(x*c_odd);
+            if(arr[j]&x)
+            c_odd=n-j-c_odd;
+        }
+    }
+    cout<<sum<<"\n";
+}
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        
+        int arr[n];
+        for(int i=0;i<n;++i)
+        cin>>arr[i];
+        XorSumSubarrays(arr,n);
+        
+    }
+}
