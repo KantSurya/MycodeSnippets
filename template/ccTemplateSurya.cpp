7eae079804fdfb692a41cf76224b470a102bc5fe
SuryaKant <suryaskant29@gmail.com>
2021-03-29 00:07:09 +0530

    template added

---
+++ template/ccTemplateSurya.cpp
@@ -0,0 +1,45 @@
+/*
+	${2:}${0:}
+	Date : 29-03-2021
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define FASTIO	ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+#define ll long long 
+#define mset(A,val) memset(A,val,sizeof(A))
+#define fi(a,b) for(int i=a;i<=b;++i)
+#define fj(a,b) for(int j=a;j<=b;++j)
+#define all(x) x.begin(),x.end()
+#define vi vector<int>
+#define pii pair<int,int>
+#define DEBUG(a...) cout<<#a<<": ";for(auto &it:a)cout<<it<<" ";cout<<"\n";
+#define debug(a)cout<<#a<<": "<<a<<"\n";
+#define int long long
+
+// ---------------------------------------------------------------------------
+const int mod = 1e9+7;
+const int maxn = 2e5 + 9;
+
+// ---------------------------------------------------------------------------
+
+// ---------------------------------------------------------------------------
+void test_case(int tc)
+{
+	// cout<<"Case #"<<tc<<": ";
+	
+}
+
+int32_t main()
+{
+	FASTIO;
+	// #ifndef ONLINE_JUDGE
+	// 	freopen("input.txt","r",stdin);
+	// 	freopen("output.txt","w",stdout);
+	// #endif
+	
+	int t=1;
+	//cin>>t;
+	for(int tc=1;tc<=t;++tc)test_case(tc);
+}
