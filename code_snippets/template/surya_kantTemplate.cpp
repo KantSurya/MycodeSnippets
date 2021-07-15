70fbd2a595c0cf84b4af19f0a18811c25cdf0040
SuryaKant <suryaskant29@gmail.com>
2021-07-15 21:58:24 +0530

    template spacing updated

--- code_snippets/template/surya_kantTemplate.cpp
+++ code_snippets/template/surya_kantTemplate.cpp
@@ -1,19 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-#define FASTIO	ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
-#define ll long long 
-#define mset(A,val) memset(A,val,sizeof(A))
-#define fi(a,b) for(int i=a;i<=b;++i)
-#define fj(a,b) for(int j=a;j<=b;++j)
-#define all(x) x.begin(),x.end()
-#define vi vector<int>
-#define pii pair<int,int>
-#define pb push_back
-#define eb emplace_back
-#define DEBUG(a...) cout<<#a<<": ";for(auto &it:a)cout<<it<<" ";cout<<endl;
-#define debug(a)cout<<#a<<": "<<a<<"\n";
-#define hola cout<<"hola"<<endl;
+#define FASTIO						ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+#define ll 							long long 
+#define mset(A,val) 				memset(A,val,sizeof(A))
+#define fi(a,b) 					for(int i=a;i<=b;++i)
+#define fj(a,b) 					for(int j=a;j<=b;++j)
+#define all(x) 						x.begin(),x.end()
+#define vi 							vector<int>
+#define pii 						pair<int,int>
+#define pb 							push_back
+#define eb 							emplace_back
+#define DEBUG(a...) 				cout<<#a<<": ";for(auto &it:a)cout<<it<<" ";cout<<endl;
+#define debug(a)					cout<<#a<<": "<<a<<"\n";
+#define hola 						cout<<"hola"<<endl;
 // #define int long long
 
 // ---------------------------------------------------------------------------
@@ -27,16 +27,15 @@ void test_case(int tc)
 {
 	// cout<<"Case #"<<tc<<": ";
 	
-	init();
 }
 
 int32_t main()
 {
 	FASTIO;
-	// #ifndef ONLINE_JUDGE
-	// 	freopen("input.txt","r",stdin);
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt","r",stdin);
 	// 	freopen("output.txt","w",stdout);
-	// #endif
+	#endif
 	
 	int t=1;
 	//cin>>t;
